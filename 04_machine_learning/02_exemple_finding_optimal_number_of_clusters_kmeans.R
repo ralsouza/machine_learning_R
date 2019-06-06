c16981a3ec42a516f0cd68f4ca480ccb1b033294
Rafael Lima <ls.rafael@icloud.com>
2019-06-06 17:20:47 -0300

    commit

--- 04_machine_learning/02_exemple_finding_optimal_number_of_clusters_kmeans.R
+++ 04_machine_learning/02_exemple_finding_optimal_number_of_clusters_kmeans.R
@@ -9,6 +9,45 @@ setwd('/home/ralsouza/Documents/r_projects/machine_learning_R/04_machine_learnin
 # Bibliotecas
 #install.packages('readr')
 library(readr)
+library(tibble)
+library(readr)
 
-### 2. Dataset ####
-# Renomeação do Arquivo
+### 2. Carga do Dataset ####
+?read.csv
+ds_SKD <- read_csv( 'StudentKnowledgeData.csv'
+                   ,col_types = cols(
+                       STG = col_double()
+                      ,SCG = col_double()
+                      ,STR = col_double()
+                      ,LPR = col_double()
+                      ,PEG = col_double()
+                      ,UNS = col_character()
+                    )
+                   )
+head(ds_SKD)
+summary(ds_SKD)
+str(ds_SKD)
+
+#### 3. Pré-Processamento ####
+# Converter Data Frame para Tibble
+?as_tibble
+ds_SKD <- as_tibble(ds_SKD)
+
+# Converter Dados Numéricos para Números
+as_tibble(ds_SKD)
+
+# Dimensão do Dataset
+dim(ds_SKD)
+
+# Validar Dados NA
+is.na(ds_SKD)
+
+# Escalar Dados
+?scale
+ds_SKD_scaled <- as.matrix(scale(ds_SKD))
+
+
+
+
+
+
